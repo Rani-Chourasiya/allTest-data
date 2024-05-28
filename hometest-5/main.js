53ce13bb81d03dabff1727e4dbcb4a8042ec4924
Rani chourasiya <ranichourasiya98@gmail.com>
2024-05-28 12:01:10 +0530

    separate some template

--- hometest-5/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleButton = document.querySelector('.toggle-button');
-    const navbarNav = document.querySelector('.navbar-nav');
-
-    toggleButton.addEventListener('click', function () {
-        navbarNav.classList.toggle('active');
-    });
-});
